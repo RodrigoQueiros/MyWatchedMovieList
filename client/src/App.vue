f5e3f73720ec6f851bc96b89e9be5323bc09e740
Rodrigo Queiros <rodrigodlq@hotmail.com>
2020-09-27 18:09:33 +0200

    Add Navbar
    Adding Navbar html and style.
    Still needs to be done: correct and adapt some styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="container">
     <Navbar />
     <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
