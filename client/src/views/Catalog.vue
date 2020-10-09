2548878d50d48c0b740c631280aba51545fc3944
Rodrigo Queiros <rodrigodlq@hotmail.com>
2020-10-09 18:09:55 +0200

    Catalog filters/search and Categories
    Development of catalog filters and search, pagination design improved
    Added images to categories and design improvment

--- client/src/views/Catalog.vue
+++ client/src/views/Catalog.vue
@@ -1,13 +1,87 @@
 <template>
   <div class="catalog">
     <div class="catalog-space">
-      <div class="catalog-box">
-        <h2 class="catalog-title">Advanced Search</h2>
+      <div>
+        <div class="catalog-box">
+          <h2 class="catalog-title">Advanced Search</h2>
+          <div class="catalog-search">
+            <div class="search-by-name-input">
+              <label for="search-by-name" class="search-label">Search for movie name</label>
+              <input
+                class="search-input"
+                type="text"
+                placeholder="Search by name"
+                name="search-by-name-input"
+                v-model="nameSearch"
+                v-on:keyup.enter="getMoviesByName()"
+              />
+              <button class="button-search" @click="getMoviesByName()">Search</button>
+            </div>
+            <div class="search-sorting">
+              <label for="search-sorting-input" class="search-label">Sort by</label>
+              <select v-model="sortSelected" name="search-sorting-input" class="select-input">
+                <option disabled value>Select one</option>
+                <option value="popularity.asc">Popularity Asc</option>
+                <option value="popularity.desc">Popularity Desc</option>
+                <option value="original_title.asc">Title Asc</option>
+                <option value="original_title.desc">Title Desc</option>
+                <option value="vote_average.asc">Rating Asc</option>
+                <option value="vote_average.desc">Rating Desc</option>
+                <option value="release_date.asc">Date Asc</option>
+                <option value="release_date.desc">Date Desc</option>
+              </select>
+            </div>
+            <div class="search-by-genre">
+              <label for="search-genre-input" class="search-label">Genre</label>
+              <select v-model="genreSelected" name="search-genre-input" class="select-input">
+                <option value>Not selected</option>
+                <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{genre.name}}</option>
+              </select>
+            </div>
+            <div class="search-by-year">
+              <label for="search-year-input" class="search-label">Year</label>
+              <input
+                class="search-input"
+                type="text"
+                placeholder="Search by year [1990-2020]"
+                name="search-year-input"
+                v-model="yearInput"
+                v-on:keyup.enter="searchMovie()"
+              />
+            </div>
+            <button class="button-search" @click="searchMovie()">Search</button>
+          </div>
+        </div>
       </div>
       <div class="catalog-box">
         <div class="catalog-header-line">
           <div>
             <h2 class="catalog-title">Catalogue</h2>
+          </div>
+          <div class="catalog-pagination-line">
+            <div>
+              <i
+                @click="goToPrevious()"
+                class="fas fa-arrow-alt-circle-left pagination-icon"
+                :style="currentPage==1?{ color: 'grey' }:''"
+              ></i>
+            </div>
+            <div class="pagination-flex">
+              <input
+                class="paginatin-input"
+                type="text"
+                v-model="currentPage"
+                v-on:keyup.enter="goToPage()"
+              />
+              <p class="pagination-text">/{{totalPages}}</p>
+            </div>
+            <div>
+              <i
+                @click="goToNext()"
+                class="fas fa-arrow-alt-circle-right pagination-icon"
+                :style="currentPage==totalPages?{ color: 'grey' }:''"
+              ></i>
+            </div>
           </div>
           <div class="catalog-views">
             <p class="catalog-views-text">views</p>
@@ -23,8 +97,16 @@
             ></i>
           </div>
         </div>
+        <div v-if="movies.length == 0">
+          <p>No movies found ):</p>
+        </div>
         <div class="catlog-cards1" v-if="viewSelected == 0">
-          <div class="catalog-card1" v-for="movie in movies" :key="movie.id">
+          <div
+            class="catalog-card1"
+            v-for="movie in movies"
+            :key="movie.id"
+            @click="goToMovie(movie.id)"
+          >
             <div class="catalog-card1-img-space">
               <img :src="movie.poster_path" alt />
             </div>
@@ -51,24 +133,30 @@
             </div>
           </div>
         </div>
-        <div class="catalog-pagination-line"></div>
-        <div>
-          <i
-            @click="goToPrevious()"
-            class="fas fa-arrow-alt-circle-left pagination-icon"
-            :style="currentPage==1?{ color: 'grey' }:''"
-          ></i>
-        </div>
-        <div>
-          <input type="text" v-model="currentPage" v-on:keyup.enter="goToPage()" />
-          <p>/{{totalPages}}</p>
-        </div>
-        <div>
-          <i
-            @click="goToNext()"
-            class="fas fa-arrow-alt-circle-right pagination-icon"
-            :style="currentPage==totalPages?{ color: 'grey' }:''"
-          ></i>
+        <div class="catalog-pagination-line">
+          <div>
+            <i
+              @click="goToPrevious()"
+              class="fas fa-arrow-alt-circle-left pagination-icon"
+              :style="currentPage==1?{ color: 'grey' }:''"
+            ></i>
+          </div>
+          <div class="pagination-flex">
+            <input
+              class="paginatin-input"
+              type="text"
+              v-model="currentPage"
+              v-on:keyup.enter="goToPage()"
+            />
+            <p class="pagination-text">/{{totalPages}}</p>
+          </div>
+          <div>
+            <i
+              @click="goToNext()"
+              class="fas fa-arrow-alt-circle-right pagination-icon"
+              :style="currentPage==totalPages?{ color: 'grey' }:''"
+            ></i>
+          </div>
         </div>
       </div>
     </div>
@@ -79,7 +167,8 @@
 //TS
 import { Component, Vue } from "vue-property-decorator";
 // Axios
-import { getMoviesAdvancedSearch } from "../API/apiDiscover";
+import { getMoviesAdvancedSearch, getMoviesByName } from "../API/apiDiscover";
+import { getAllGenres } from "../API/apiGenre";
 
 type movieType = {
   overview: string;
@@ -91,13 +180,24 @@ type movieType = {
   src: string;
 };
 
+type genreType = {
+  name: string;
+  id: number;
+};
+
 @Component({
   components: {}
 })
 export default class Catalog extends Vue {
+  private searchSelected: number = 0;
   private viewSelected: number = 0;
   private totalPages: number = 50;
   private currentPage: number = 1;
+  private sortSelected: string = "popularity.desc";
+  private genreSelected: string = "";
+  private yearInput: string = "";
+  private nameSearch: string = "";
+  private genres: genreType[] = [];
   private movies: movieType[] = [];
   //   Css class
   private selected: string = "view-selected";
@@ -107,7 +207,16 @@ export default class Catalog extends Vue {
     if (localStorage.getItem("view")) {
       this.viewSelected = parseInt(localStorage.getItem("view"));
     }
-    this.getMovies("&page=" + this.currentPage);
+    if (this.$route.params.movieId) {
+      this.genreSelected = this.$route.params.movieId;
+    }
+    this.getMovies();
+
+    //Get all genres
+
+    getAllGenres().then(response => {
+      this.genres = response.data.genres;
+    });
   }
   public goToMovie(movieId): void {
     //Go to the details page of movie
@@ -120,7 +229,52 @@ export default class Catalog extends Vue {
     localStorage.setItem("view", changeTo);
   }
 
-  public getMovies(query: string): void {
+  public getMoviesByName(): void {
+    this.searchSelected = 1;
+    let query = "&query=" + this.nameSearch + "&page=" + this.currentPage;
+    console.log(query);
+    getMoviesByName(query).then(response => {
+      this.movies = response.data.results;
+      this.totalPages = response.data.total_pages;
+
+      for (let i = 0; i < 20; i++) {
+        //Correct the path
+        this.movies[i].poster_path =
+          "https://image.tmdb.org/t/p/w500" + this.movies[i].poster_path;
+
+        if (this.movies[i].overview.length >= 300) {
+          this.movies[i].overview =
+            this.movies[i].overview.substr(0, 300) + "...";
+        }
+
+        if (this.movies[i].title.length >= 32) {
+          this.movies[i].title = this.movies[i].title.substr(0, 32) + "...";
+        }
+      }
+    });
+  }
+
+  public getMovies(): void {
+    let query =
+      "&sort_by=" +
+      this.sortSelected +
+      "&page=" +
+      this.currentPage +
+      "&vote_count.gte=100";
+
+    if (this.genreSelected != "") {
+      query = query + "&with_genres=" + this.genreSelected;
+    }
+    if (
+      this.yearInput != "" &&
+      parseInt(this.yearInput) <= 2020 &&
+      parseInt(this.yearInput) >= 1990
+    ) {
+      query = query + "&primary_release_year=" + this.yearInput;
+    }
+
+    console.log(query);
+
     getMoviesAdvancedSearch(query).then(response => {
       this.movies = response.data.results;
       this.totalPages = response.data.total_pages;
@@ -146,48 +300,42 @@ export default class Catalog extends Vue {
   public goToPrevious(): void {
     if (this.currentPage > 1) {
       this.currentPage -= 1;
-      this.getMovies("&page=" + this.currentPage);
+      if (this.searchSelected == 0) {
+        this.getMovies();
+      } else {
+        this.getMoviesByName();
+      }
     }
   }
 
   public goToNext(): void {
     if (this.currentPage < this.totalPages) {
       this.currentPage += 1;
-      this.getMovies("&page=" + this.currentPage);
+      if (this.searchSelected == 0) {
+        this.getMovies();
+      } else {
+        this.getMoviesByName();
+      }
     }
   }
 
   public goToPage(): void {
     console.log(this.currentPage);
     if (this.currentPage >= 1 && this.currentPage <= this.totalPages) {
-      this.getMovies("&page=" + this.currentPage);
+      if (this.searchSelected == 0) {
+        this.getMovies();
+      } else {
+        this.getMoviesByName();
+      }
     }
   }
+
+  public searchMovie(): void {
+    this.nameSearch = "";
+    this.searchSelected = 0;
+    this.currentPage = 1;
+    this.getMovies();
+  }
 }
-
-/*
-&sort_by=
-popularity.asc
-popularity.desc
-release_date.asc
-release_date.desc
-original-title.asc
-original-title.desc
-vote_average.asc
-vote_average.desc
-vote_count.asc
-vote_count.desc
-
-page=1
-
-with_genres=
-id
-
-primary_release_year=
-
-vote_count.gte=100
-
-
-*/
 </script>
 
--- client/src/views/Categories.vue
+++ client/src/views/Categories.vue
@@ -2,8 +2,20 @@
   <div class="categories">
     <h1 class="categories-header">Categories</h1>
     <div class="categories-name">
-      <div class="categories-space" v-for="genre in genres" :key="genre.id">
-        <p class="categories-text">{{genre.name}}</p>
+      <div
+        class="categories-space"
+        v-for="genre in genres"
+        :key="genre.id"
+        @click="goToGenre(genre.id)"
+      >
+        <div class="categories-gradient" :style="{ color: genre.color}">
+          <img
+            v-bind:src="require('../assets/categories/' + genre.img)"
+            alt
+            class="categories-card-img"
+          />
+          <p class="categories-text">{{genre.name}}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -18,6 +30,13 @@ import { getAllGenres } from "../API/apiGenre";
 type genreType = {
   name: string;
   id: number;
+  img: string;
+  color: string;
+};
+
+type genreImage = {
+  name: string;
+  color: string;
 };
 
 @Component({
@@ -25,14 +44,97 @@ type genreType = {
 })
 export default class CategoriesPage extends Vue {
   private genres: genreType[] = [];
+  private images: genreImage[] = [
+    {
+      name: "action.jpg",
+      color: "rgba(200, 100, 0, 0.6)"
+    },
+    {
+      name: "adventure.jpg",
+      color: "rgba(50, 50, 150, 0.6)"
+    },
+    {
+      name: "animation.jpg",
+      color: "rgba(150, 150, 20, 0.6)"
+    },
+    {
+      name: "comedy.jpg",
+      color: "rgba(50, 200, 100, 0.6)"
+    },
+    {
+      name: "crime.jpg",
+      color: "rgba(100, 0, 100, 0.6)"
+    },
+    {
+      name: "documentary.jpg",
+      color: "rgba(100, 30, 10, 0.6)"
+    },
+    {
+      name: "drama.jpg",
+      color: "rgba(200, 150, 200, 0.6)"
+    },
+    {
+      name: "family.jpg",
+      color: "rgba(70, 155, 170, 0.6)"
+    },
+    {
+      name: "fantasy.jpg",
+      color: "rgba(200, 50, 180, 0.6)"
+    },
+    {
+      name: "history.jpg",
+      color: "rgba(120, 100, 60, 0.6)"
+    },
+    {
+      name: "horror.jpg",
+      color: "rgba(80, 5, 20, 0.6)"
+    },
+    {
+      name: "music.jpg",
+      color: "rgba(150, 150, 100, 0.6)"
+    },
+    {
+      name: "mystery.jpg",
+      color: "rgba(30, 80, 40, 0.6)"
+    },
+    {
+      name: "romance.jpg",
+      color: "rgba(200, 20, 20, 0.6)"
+    },
+    {
+      name: "science.jpg",
+      color: "rgba(80, 5, 220, 0.6)"
+    },
+    {
+      name: "tvmovie.jpg",
+      color: "rgba(220, 125, 70, 0.6)"
+    },
+    {
+      name: "thriller.jpg",
+      color: "rgba(70, 20, 20, 0.6)"
+    },
+    {
+      name: "war.jpg",
+      color: "rgba(130, 105, 100, 0.6)"
+    },
+    {
+      name: "western.jpg",
+      color: "rgba(200, 150, 50, 0.6)"
+    }
+  ];
   created() {
     getAllGenres().then(response => {
       this.genres = response.data.genres;
+      for (let i = 0; i < this.genres.length; i++) {
+        this.genres[i].img = this.images[i].name;
+        this.genres[i].color = this.images[i].color;
+      }
+      console.log(this.genres);
     });
   }
   public goToGenre(movieId): void {
     //Go to the details page of movie
-    this.$router.push({ path: "/catalog/" + movieId });
+    this.$router.push({ name: "Catalog", params: { movieId } });
   }
 }
 </script>
